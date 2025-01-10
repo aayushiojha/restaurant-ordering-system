0619a0118a12bfd9d595137f0c2467080b9e0292
Aayushi <aayushiojha13@gmail.com>
2025-01-10 22:28:19 +0530

    Update server to listen on all interfaces (0.0.0.0)

--- backend/index.js
+++ backend/index.js
@@ -51,6 +51,6 @@ app.get("/*", function (req, res) {
 
 // PORT
 const PORT = process.env.PORT || 8001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
