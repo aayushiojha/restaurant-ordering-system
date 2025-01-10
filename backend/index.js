45273bd49b3b48914ee7720b5204c09cca26f077
Aayushi <aayushiojha13@gmail.com>
2025-01-10 23:22:27 +0530

    Update server

--- backend/index.js
+++ backend/index.js
@@ -51,6 +51,6 @@ app.get("/*", function (req, res) {
 
 // PORT
 const PORT = process.env.PORT || 8001;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
